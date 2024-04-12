2e803eb6109b651ec6a6910568e77f5e456fa2d5
Tani-NT <114972865+Tani-NT@users.noreply.github.com>
2024-04-12 14:18:35 +0530

    chatbot UI interface done

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Message from './components/Message';
+import Title from './components/Title';
+import Response from './components/Response';
+import Filter from './components/Filter';
+import Input from './components/Input';
+
+function App() {
+  return(
+    <>
+      <Title/>
+      <Message/>
+      <Response/>
+      <Filter/>
+      <Input/>
+    </>
+  )
+}
+
+export default App;
